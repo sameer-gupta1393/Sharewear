c40f8da94e5293fcf7ae5f6a72341c2d05e9c845
swayamName1 <swayamrana808@gmail.com>
2024-02-01 22:37:22 +0530

    page 1 initialised

---
+++ frontend/src/components/parts/Cards.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const Cards = () => {
+  return (
+    <div className="max-w-sm rounded overflow-hidden shadow-lg h-[300px]">
+     <img className="w-full h-[50%] object-cover object-top" src="https://projectbandi.com/cdn/shop/products/IMG_5995.jpg?v=1573801843" alt="Sunset in the mountains"/>
+  <div className="px-6 py-4">
+    <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+    <p className="text-gray-700 text-base">
+      Lorem ipsum dolor sit amet.
+    </p>
+  </div>
+  <div className="px-6 pt-4  flex justify-between">
+        <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            Rent it
+             
+        </a>
+        <a className='cursor-pointer p-2  '>
+                <svg className="w-5 h-5 text-red-500 hover:text-red-700  dark:text-white-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20">
+                  <path d="M17 5.923A1 1 0 0 0 16 5h-3V4a4 4 0 1 0-8 0v1H2a1 1 0 0 0-1 .923L.086 17.846A2 2 0 0 0 2.08 20h13.84a2 2 0 0 0 1.994-2.153L17 5.923ZM7 9a1 1 0 0 1-2 0V7h2v2Zm0-5a2 2 0 1 1 4 0v1H7V4Zm6 5a1 1 0 1 1-2 0V7h2v2Z"/>
+               </svg>
+
+        </a>
+        
+     
+  </div>
+</div>
+  )
+}
+
+export default Cards
