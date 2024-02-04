c085b621efdeb7cadf306e5713ac92af85b46138
swayamName1 <swayamrana808@gmail.com>
2024-02-04 23:07:04 +0530

    page 3.2

--- frontend/src/components/Page3.jsx
+++ frontend/src/components/Page3.jsx
@@ -16,21 +16,6 @@ export default function Page3() {
       <i className="fa-solid fa-arrow-left cursor-pointer "></i>
       <h1 className="grow p-20 font-bold">POST YOUR AD</h1>
     </nav>
-
-    {/* <div className="h-auto mt-[30px] bg-white rounded-lg w-3/5 m-auto border-black border-2">
-          <h6 className="p-5 border-b-2 border-black/20 font-bold">CHOOSE A CATEGORY</h6><hr className="h-[5px]"/>
-          {catagories.map((item,id)=>{
-            return <TabButton click={()=>{handleCLick(id)}} key={id}>{item}</TabButton>
-          })}
-    </div>
-
-    <section>
-      <div><h2>SELECTED CATEGORY</h2>
-      <p> {catagories.filter((item,id)=>{
-            return  cate==id
-          })}</p>
-      </div>
-    </section> */}
   
     <TabButton />
 
